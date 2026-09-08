d017a79b510dc59952ce3e4d3b856832b0189762
001254158 <Chamila.Mudiyanselage@student.tafesa.edu.au>
2026-09-08 11:34:32 +0930

    provide further 10% discount to all ticket types.

--- Ticketing/Ticketing/TicketPrice.cs
+++ Ticketing/Ticketing/TicketPrice.cs
@@ -17,7 +17,7 @@ namespace Ticketing
         const decimal mdecBalcony = 47m;
         const decimal mdecGeneral = 37m;
         const decimal mdecBox = 75m;
-        const decimal mdecDiscount = 5.0m; 
+        const decimal mdecDiscount = 15.0m; 
 
         private int Section
         {
